af8a896ef99ccb6592993ba13358e6562c8f2ad0
gaurav-acco <gaurav-acco>
2024-05-31 19:04:31 +0530

    comit gaurav

---
+++ t1.sql
@@ -0,0 +1,16 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      Branch_test.DM1
+ *
+ * Date Created : Friday, May 31, 2024 19:04:31
+ * Target DBMS : Databricks
+ */
+
+CREATE TABLE Entity1
+(
+    id      string      NOT NULL,
+    name    string,
+    CONSTRAINT PK1 PRIMARY KEY (id) 
+)
+;
+
