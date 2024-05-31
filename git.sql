c18bed38b0dea808bb5cd996da16b00f732d7077
prema.v <prema.v>
2024-05-31 15:56:46 +0530

    hi git

---
+++ git.sql
@@ -0,0 +1,22 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      computed1.DM1
+ *
+ * Date Created : Friday, May 31, 2024 15:56:46
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: Entity1 
+ */
+
+CREATE TABLE Entity1
+(
+    testA    string,
+    testB    string,
+    testC    string,
+    testD    string,
+    testE    string
+)
+;
+
