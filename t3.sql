26596b7f7c5caa96d196f90c4a2dc52d1cb78679
prema.v <prema.v>
2024-05-31 19:09:04 +0530

    hi t3

---
+++ t3.sql
@@ -0,0 +1,20 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      Branch_test.DM1
+ *
+ * Date Created : Friday, May 31, 2024 19:09:04
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: Entity1 
+ */
+
+CREATE TABLE Entity1
+(
+    id      string      NOT NULL,
+    name    string,
+    CONSTRAINT PK1 PRIMARY KEY (id) 
+)
+;
+
