e56a63696c06d6c0b9a015421eb5b52a749f8378
gaurav-acco <gaurav-acco>
2024-06-06 16:10:26 +0530

    dd commit

--- branch1.sql
+++ branch1.sql
@@ -1,25 +1,6 @@
-/*
- * ER/Studio Data Architect SQL Code Generation
- * Project :      Branch_test.DM1
- *
- * Date Created : Friday, May 31, 2024 15:31:12
- * Target DBMS : Microsoft SQL Server 2019
- */
 
-CREATE TABLE [Entity1](
-    [id]        char(10)    NOT NULL,
-    [name]      char(10)    NULL,
-    [order_id]  char(10)    NULL,
-    [amount]    char(10)    NULL,
-    CONSTRAINT [PK1] PRIMARY KEY NONCLUSTERED ([id])
-)
+-- Standard Alter Table SQL
 
-go
-
-
-IF OBJECT_ID('Entity1') IS NOT NULL
-    PRINT '<<< CREATED TABLE Entity1 >>>'
-ELSE
-    PRINT '<<< FAILED CREATING TABLE Entity1 >>>'
+ALTER TABLE dbo.Entity1 ADD dd char(10) NULL
 go
 
