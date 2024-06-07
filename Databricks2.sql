a7a002602ac48b10875a54a39b93986c82cb3b8f
itsAnkitcode001 <itsAnkitcode001>
2024-06-07 10:24:13 +0530

    fg

--- Databricks2.sql
+++ Databricks2.sql
@@ -1,14 +1,44 @@
 /*
  * ER/Studio Data Architect SQL Code Generation
+ * Project :      Students_Copy.DM1
  *
- * Date Created : Friday, June 07, 2024 10:22:58
+ * Date Created : Friday, June 07, 2024 10:24:13
  * Target DBMS : Google BigQuery
  */
 
-/*
- * ER/Studio Data Architect SQL Code Generation
- *
- * Date Created : Friday, June 07, 2024 10:23:19
- * Target DBMS : Google BigQuery
+/* 
+ * TABLE: tbl_Students 
  */
+
+CREATE TABLE tbl_Students(
+    StudentId         INTEGER       NOT NULL,
+    Firstname         STRING(18),
+    Lastname          STRING(18)    NOT NULL,
+    Email             STRING(18)    NOT NULL,
+    DOB               DATE          NOT NULL,
+    AverageScore      INTEGER,
+    gstest_Address    STRUCT<
+        Country    STRING(18)    NOT NULL,
+        City       STRING(18)    NOT NULL,
+        CAP        INTEGER       NOT NULL
+    >
+)
+OPTIONS( 
+    friendly_name = "tbl_Students"
+)
+;
+
+/* 
+ * TABLE: Test 
+ */
+
+CREATE TABLE Test(
+    col1    STRING(18),
+    col2    STRING(18),
+    col3    STRING(18)
+)
+OPTIONS( 
+    friendly_name = "Entity1"
+)
+;
 
