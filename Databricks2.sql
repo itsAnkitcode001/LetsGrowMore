3e2f21a7f2c997f2529c3230314300f0b1d3299b
itsAnkitcode001 <itsAnkitcode001>
2024-06-07 10:23:19 +0530

    ankit

---
+++ Databricks2.sql
@@ -0,0 +1,14 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ *
+ * Date Created : Friday, June 07, 2024 10:22:58
+ * Target DBMS : Google BigQuery
+ */
+
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ *
+ * Date Created : Friday, June 07, 2024 10:23:19
+ * Target DBMS : Google BigQuery
+ */
+
