157c724e51c89bc33c5567ce58b528c332e966c2
itsAnkitcode001 <itsAnkitcode001>
2024-06-07 12:17:48 +0530

    ankit

--- array.sql
+++ array.sql
@@ -1,14 +1,14 @@
 /*
  * ER/Studio Data Architect SQL Code Generation
  *
- * Date Created : Friday, June 07, 2024 12:17:23
+ * Date Created : Friday, June 07, 2024 12:17:48
  * Target DBMS : Databricks
  */
 
 /*
  * ER/Studio Data Architect SQL Code Generation
  *
- * Date Created : Friday, June 07, 2024 12:17:23
+ * Date Created : Friday, June 07, 2024 12:17:48
  * Target DBMS : Databricks
  */
 
