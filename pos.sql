577bce288b15f53f0a98a6bf8c9644f6556ced11
itsAnkitcode001 <itsAnkitcode001>
2024-06-07 12:05:33 +0530

    ankit

--- pos.sql
+++ pos.sql
@@ -1,19 +1,14 @@
 --
 -- ER/Studio Data Architect SQL Code Generation
--- Project :      Model1_1.DM1
 --
--- Date Created : Friday, June 07, 2024 10:12:37
+-- Date Created : Friday, June 07, 2024 12:05:32
 -- Target DBMS : PostgreSQL 10.x-12.x
 --
 
--- 
--- TABLE: "Entity1" 
 --
-
-CREATE TABLE "Entity1"(
-
-)
-;
-
-
+-- ER/Studio Data Architect SQL Code Generation
+--
+-- Date Created : Friday, June 07, 2024 12:05:33
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
 
