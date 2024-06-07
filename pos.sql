8cc85e3e866c30a1771389d7d2090695b0625f16
itsAnkitcode001 <itsAnkitcode001>
2024-06-07 10:12:37 +0530

    push

---
+++ pos.sql
@@ -0,0 +1,19 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1_1.DM1
+--
+-- Date Created : Friday, June 07, 2024 10:12:37
+-- Target DBMS : PostgreSQL 10.x-12.x
+--
+
+-- 
+-- TABLE: "Entity1" 
+--
+
+CREATE TABLE "Entity1"(
+
+)
+;
+
+
+
