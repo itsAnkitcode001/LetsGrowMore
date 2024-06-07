e5920f74a76920eada31135e862b8c32b6ef71e3
itsAnkitcode001 <itsAnkitcode001>
2024-06-07 12:13:00 +0530

    ankit

--- Databricks2.sql
+++ Databricks2.sql
@@ -1,44 +1,14 @@
 /*
  * ER/Studio Data Architect SQL Code Generation
- * Project :      Students_Copy.DM1
  *
- * Date Created : Friday, June 07, 2024 10:24:13
+ * Date Created : Friday, June 07, 2024 12:12:59
  * Target DBMS : Google BigQuery
  */
 
-/* 
- * TABLE: tbl_Students 
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ *
+ * Date Created : Friday, June 07, 2024 12:13:00
+ * Target DBMS : Google BigQuery
  */
-
-CREATE TABLE tbl_Students(
-    StudentId         INTEGER       NOT NULL,
-    Firstname         STRING(18),
-    Lastname          STRING(18)    NOT NULL,
-    Email             STRING(18)    NOT NULL,
-    DOB               DATE          NOT NULL,
-    AverageScore      INTEGER,
-    gstest_Address    STRUCT<
-        Country    STRING(18)    NOT NULL,
-        City       STRING(18)    NOT NULL,
-        CAP        INTEGER       NOT NULL
-    >
-)
-OPTIONS( 
-    friendly_name = "tbl_Students"
-)
-;
-
-/* 
- * TABLE: Test 
- */
-
-CREATE TABLE Test(
-    col1    STRING(18),
-    col2    STRING(18),
-    col3    STRING(18)
-)
-OPTIONS( 
-    friendly_name = "Entity1"
-)
-;
 
