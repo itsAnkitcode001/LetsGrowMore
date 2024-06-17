563d216d0bf552bc5969c02016078bca916dc115
itsAnkitcode001 <itsAnkitcode001>
2024-06-17 14:01:44 +0530

    dawqd

---
+++ Databricks2.sql
@@ -0,0 +1,20 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      Students_Copy.DM1
+ *
+ * Date Created : Monday, June 17, 2024 14:01:44
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: Test 
+ */
+
+CREATE TABLE Test
+(
+    col1    string,
+    col2    string,
+    col3    string
+)
+;
+
