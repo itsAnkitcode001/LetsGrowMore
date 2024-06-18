deee8efa868d4b319e0d40dede3401e79cdd192e
itsAnkitcode001 <itsAnkitcode001>
2024-06-19 00:13:02 +0530

    hkj

--- Databricks2_ALTER.sql
+++ Databricks2_ALTER.sql
@@ -1,11 +1,32 @@
 
 -- Drop Constraint, Rename and Create Table SQL
 
+ALTER TABLE Test
+    RENAME TO Test_d62ba558
+
+;
+
 CREATE TABLE gstest.Test
 (
     col1 string,
     col2 string,
     col3 string
 )
+
+;
+
+INSERT INTO gstest.Test
+( col1,
+  col2,
+  col3 ) 
+SELECT
+col1,
+col2,
+col3
+FROM Test_d62ba558
+
+;
+
+DROP TABLE IF EXISTS Test_d62ba558
 
 ;
