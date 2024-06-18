8443a02543592ea11a7293e20403568dc9936ba9
itsAnkitcode001 <itsAnkitcode001>
2024-06-18 22:08:26 +0530

    gjhbn

---
+++ n.sql
@@ -0,0 +1,22 @@
+--
+-- ER/Studio Data Architect SQL Code Generation
+-- Project :      Model1git.DM1
+--
+-- Date Created : Tuesday, June 18, 2024 22:08:26
+-- Target DBMS : Snowflake
+--
+
+-- 
+-- TABLE: Entity1 
+--
+
+CREATE TABLE Entity1(
+    z     CHAR(10),
+    a     CHAR(10),
+    b     CHAR(10),
+    cd    CHAR(10)
+)
+;
+
+
+
