8d97105c10a3da7a49abd91e9517eba69a9df826
itsAnkitcode001 <itsAnkitcode001>
2024-06-18 19:49:14 +0530

    yhg

---
+++ sxd534.sql
@@ -0,0 +1,20 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      Students_Copy.DM1
+ *
+ * Date Created : Tuesday, June 18, 2024 19:49:14
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: Test 
+ */
+
+CREATE TABLE Test
+(
+    col1    string,
+    col2    string,
+    col3    string
+)
+;
+
