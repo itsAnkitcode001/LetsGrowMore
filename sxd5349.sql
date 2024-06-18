6463f42213e4a2cfd76d0066b8408b8a1bf9e656
itsAnkitcode001 <itsAnkitcode001>
2024-06-18 19:56:50 +0530

    hgvnb123abkigfhjfghjklhghjsfhg

--- sxd5349.sql
+++ sxd5349.sql
@@ -1,7 +1,7 @@
 /*
  * ER/Studio Data Architect SQL Code Generation
  *
- * Date Created : Tuesday, June 18, 2024 19:55:27
+ * Date Created : Tuesday, June 18, 2024 19:56:50
  * Target DBMS : Databricks
  */
 
