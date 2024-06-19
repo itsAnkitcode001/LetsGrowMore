4b821fbbee57d20d14071660a181998650e94e87
itsAnkitcode001 <itsAnkitcode001>
2024-06-19 18:12:29 +0530

    a

---
+++ abc.sql
@@ -0,0 +1,18 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ * Project :      Model1.DM1
+ *
+ * Date Created : Wednesday, June 19, 2024 18:12:29
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: Entity1 
+ */
+
+CREATE TABLE Entity1
+(
+
+)
+;
+
