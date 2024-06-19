ba30c3b7fd00213a6b7eb15a1f1e1ee4cd5d4924
itsAnkitcode001 <itsAnkitcode001>
2024-06-19 20:00:15 +0530

    hgvnb123abkis

---
+++ mainuu.sql
@@ -0,0 +1,19 @@
+/*
+ * ER/Studio Data Architect SQL Code Generation
+ *
+ * Date Created : Wednesday, June 19, 2024 20:00:15
+ * Target DBMS : Databricks
+ */
+
+/* 
+ * TABLE: Test 
+ */
+
+CREATE TABLE Test
+(
+    col1    string,
+    col2    string,
+    col3    string
+)
+;
+
