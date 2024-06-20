4d5e5d1f1c4b1acbbe4e0fd50f4e0a60187988e3
itsAnkitcode001 <itsAnkitcode001>
2024-06-20 11:30:04 +0530

    hgvnb123abkis

--- yu.sql
+++ yu.sql
@@ -1,7 +1,7 @@
 /*
  * ER/Studio Data Architect SQL Code Generation
  *
- * Date Created : Thursday, June 20, 2024 11:17:31
+ * Date Created : Thursday, June 20, 2024 11:29:47
  * Target DBMS : Databricks
  */
 
