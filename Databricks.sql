3b199ba00758a82308e07054f9b8b41d662fbe31
itsAnkitcode001 <itsAnkitcode001>
2024-06-20 21:36:01 +0530

    hhhhhb

--- Databricks.sql
+++ Databricks.sql
@@ -2,7 +2,7 @@
  * ER/Studio Data Architect SQL Code Generation
  * Project :      Students_Copy.DM1
  *
- * Date Created : Tuesday, June 18, 2024 14:58:49
+ * Date Created : Thursday, June 20, 2024 21:35:58
  * Target DBMS : Databricks
  */
 
