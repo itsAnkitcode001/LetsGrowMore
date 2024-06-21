a1823f8169efa2d571b2002cf3010af7f7aafbb1
itsAnkitcode001 <itsAnkitcode001>
2024-06-21 12:51:50 +0530

    skjhgkjhgbvcjk

---
+++ sample_ALTER.sql
@@ -0,0 +1,17 @@
+
+-- Drop Constraint, Rename and Create Table SQL
+
+DROP TABLE IF EXISTS gstest.tbl_Students
+
+;
+DROP TABLE IF EXISTS gstest.Address
+
+;
+CREATE TABLE gstest.Test
+(
+    col1 string,
+    col2 string,
+    col3 string
+)
+
+;
